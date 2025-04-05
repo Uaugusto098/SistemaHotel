12efc1e51eb6b065f1b1ed7917c37431a81debc1
Pedro augusto <pedroaugusto.borges36@gmail.com>
2025-04-05 14:27:46 -0300

    Arquivo sistema do hotel 05/04

---
+++ Hotel.py
@@ -0,0 +1,8 @@
+print ("-------Sistema de um Hotel-------")
+print ("\n")
+
+
+
+cliente1=input("Digite seu nome: ","\nDigite sua idade: ")
+cliente2=input("Digite seu nome: ","\nDigite sua idade: ")
+cliente3=input("Digite seu nome: ","\nDigite sua idade: ")
